7aa8cb1bc02be28bfb77808eec9158041fffb35f
rcdraft <19248934+rcdraft@users.noreply.github.com>
2020-07-03 14:53:02 +0800

    Added end callout interaction

--- HotCalloutsV/Entities/MenuFiber.cs
+++ HotCalloutsV/Entities/MenuFiber.cs
@@ -1,19 +1,11 @@
 ﻿// Copyright (C) RelaperCrystal 2019, 2020
 // This file is part of HotCallouts for Grand Theft Auto V.
 
-// NO COMPILE OF THIS FILE
-// As an result, no class can be created.
-// I am still working on how to get user input.
-// It can be used as call onscreen keyboard.
-
-/*using Rage;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using Rage;
 using RAGENativeUI;
 using RAGENativeUI.Elements;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotCalloutsV.Entities
 {
@@ -21,27 +13,25 @@ namespace HotCalloutsV.Entities
     {
         private static MenuPool pool;
         private static UIMenu menu;
-        private static UIMenuItem detain;
+        private static UIMenuItem itemEndCall;
         private static UIMenuItem arrest;
 
         public static void CreateLoop()
         {
             pool = new MenuPool();
-            menu = new UIMenu("HotCallouts", "Control Menu");
-            detain = new UIMenuItem("Create Detain Report", "Creates an detain report.");
-            arrest = new UIMenuItem("Create Arrest", "Reports an arrest.");
-            menu.AddItem(detain);
-            menu.AddItem(arrest);
+            menu = new UIMenu("HotCallouts", "Interaction Menu");
+            itemEndCall = new UIMenuItem("End Call", "Ends current callout. All units will disengage and suspects will go away.");
+            menu.AddItem(itemEndCall);
             menu.RefreshIndex();
-            detain.Activated += Detain_Activated;
-            arrest.Activated += Arrest_Activated;
+            itemEndCall.Activated += Detain_Activated;
             pool.Add(menu);
             while(true)
             {
                 pool.ProcessMenus();
-                if(Game.IsKeyDown(System.Windows.Forms.Keys.F9) && Game.IsKeyDown(System.Windows.Forms.Keys.LControlKey))
+                if(Game.IsKeyDown(System.Windows.Forms.Keys.Y) && Game.IsKeyDown(System.Windows.Forms.Keys.LControlKey))
                 {
-                    if(!pool.IsAnyMenuOpen())
+                    itemEndCall.Enabled = Functions.IsCalloutRunning();
+                    if (!pool.IsAnyMenuOpen())
                     {
                         menu.Visible = !menu.Visible;
                     }
@@ -50,15 +40,14 @@ namespace HotCalloutsV.Entities
             }
         }
 
-        private static void Arrest_Activated(UIMenu sender, UIMenuItem selectedItem)
-        {
-            
-        }
-
         private static void Detain_Activated(UIMenu sender, UIMenuItem selectedItem)
         {
-            
+            if(Functions.IsCalloutRunning())
+            {
+                Game.LogTrivial("Player is ending " + Functions.GetCalloutName(Functions.GetCurrentCallout()) + "by force.");
+                Functions.StopCurrentCallout();
+                itemEndCall.Enabled = false;
+            }
         }
     }
 }
-*/
